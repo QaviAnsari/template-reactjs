7e509984eb0a13e362b7548fb658d36aa91035da
QaviAnsari <86542352+QaviAnsari@users.noreply.github.com>
2022-06-30 23:50:36 +0530

    template
    just front end simple template using reactjs +tailwind

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Buy from "./components/Buy";
+import CTA from "./components/CTA";
+import Five from "./components/Five";
+import Footer1 from "./components/Footer1";
+import Gallery from "./components/Gallery";
+import Stats from "./components/Stats";
+import Team from "./components/Team";
+import Team2 from "./components/Team2";
+import Four from "./components/Four";
+import Nav1 from "./components/Nav1";
+import Sec2 from "./components/Sec2";
+import Stat3 from "./components/Stat3";
+import Buy3 from "./components/Buy3";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav1 />
+      <Sec2 />
+      <Stats />
+      <Gallery />
+      <Four />
+       <Stat3 />
+      <Five />
+      <Buy3 />
+      <Team />
+      <Team2 />
+      <Buy />
+      <Footer1 />
+      <CTA />
+    </div>
+  );
+}
+
+export default App;
